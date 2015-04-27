c88fc066759d2b494857631a9cdfa897e4207271
Vivekanand <Vivekanandayya@gmail.com>
2015-04-27 12:45:44 +0530

    session changes

---
+++ MvcMusicStore-Completed/MvcMusicStore/Controllers/SecureController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class SecureController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            if (Session["Logged"] == null)
+                return RedirectToAction("LogOn", "Account");
+
+            return View();
+        }
+    }
+}
