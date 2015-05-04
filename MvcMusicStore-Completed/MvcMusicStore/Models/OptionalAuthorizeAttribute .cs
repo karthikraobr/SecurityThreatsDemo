e09529179102896e79c58ec794a589fd371ecccb
swodeshm <swodeshmuduli@gmail.com>
2015-05-04 07:10:54 -0700

    Authorization Bypassing Cross Site Request Forgery Missing Anti-Scripting Mechanism changes

---
+++ MvcMusicStore-Completed/MvcMusicStore/Models/OptionalAuthorizeAttribute .cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Models
+{
+    public class OptionalAuthorizeAttribute : AuthorizeAttribute
+    {
+        private readonly bool _authorize;
+
+        public OptionalAuthorizeAttribute()
+        {
+            _authorize = true;
+        }
+
+        public OptionalAuthorizeAttribute(bool authorize)
+        {
+            _authorize = authorize;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (!_authorize)
+                return true;
+
+            return base.AuthorizeCore(httpContext);
+        }
+    }
+}
